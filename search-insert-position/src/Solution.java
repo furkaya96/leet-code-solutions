b5eb81d683c5e344e81893ae1da20856310fdaa7
furkankaya <furkaya06@gmail.com>
2022-01-20 12:01:21 +0300

    Search insert position problem is solved

---
+++ search-insert-position/src/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (target == nums[mid])
+                return mid;
+            if (target < nums[mid])
+                high = mid - 1;
+            if (target > nums[mid])
+                low = mid + 1;
+        }
+        return low;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1, 3, 5, 6};
+        int target = 5;
+        System.out.println(solution.searchInsert(nums, target));
+    }
+}
